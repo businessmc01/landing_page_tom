5854b6f41a7d6b7c8a299772a335bb57b0e54494
businessmc01 <91619324+businessmc01@users.noreply.github.com>
2021-10-01 13:34:00 -0300

    Add files via upload

--- assets/source/app.js
+++ assets/source/app.js
@@ -2,7 +2,6 @@ const faqs = document.querySelectorAll(".question")
 
 
 faqs.forEach(faq =>{
-    console.log(faq)
     faq.addEventListener("click",()=>{        
         faqs.forEach(function(answer){
             if (faq !== answer) {
@@ -13,3 +12,41 @@ faqs.forEach(faq =>{
         faq.parentElement.classList.toggle("active");
     })
 })
+
+/*Carousel Script*/
+const caroselImages = document.querySelector(".caroseul-container")
+       const carouselButtons = document.querySelectorAll(".carousel_button")
+       const numberOfImages = document.querySelectorAll(".caroseul-container img").length
+      
+       let imageIndex = 1
+       let translateX = 0
+
+       
+
+       carouselButtons.forEach(button => {
+           button.addEventListener("click",(ev) =>{
+               if (event.target.id == "previous"){
+                   if(imageIndex == 1){
+                       imageIndex = numberOfImages
+                       translateX -= parseFloat(getComputedStyle(document.querySelector(".carousel")).width)*(numberOfImages-1)
+
+                   } else if (imageIndex !== 1){
+                       imageIndex--;
+                       translateX += parseFloat(getComputedStyle(document.querySelector(".carousel")).width);
+                   }
+               } else {
+                    
+                   if (imageIndex == numberOfImages){
+                       imageIndex = 1
+                       translateX += parseFloat(getComputedStyle(document.querySelector(".carousel")).width)*(numberOfImages-1)
+
+                   } else {
+                       imageIndex++
+                       translateX -=parseFloat(getComputedStyle(document.querySelector(".carousel")).width)
+                   }
+               }
+               caroselImages.style.transform = `translateX(${translateX}px`
+           })
+       })
+
+/*Fim carousel*/
